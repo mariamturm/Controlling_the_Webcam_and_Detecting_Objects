0185babd1405ebe78e2d5546f58605eb2d2c325a
mariam <mariam@kompishka.local>
2025-11-10 16:00:47 +0400

    Storing object detection timestamps in CSV and JSON files

--- code.py
+++ code.py
@@ -1,8 +1,14 @@
+from datetime import datetime
 import time
 import cv2
+import pandas
 
 first_frame = None
 #frame_count = 0
+status_list = [None, None]
+times = []
+
+df = pandas.DataFrame(columns = ["Start", "End"])
 
 video = cv2.VideoCapture(1)
 time.sleep(0.07)
@@ -11,6 +17,7 @@ time.sleep(0.07)
 while True:
     #a = a+1
     check, frame = video.read()
+    status = 0
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray,(21,21),0)
@@ -29,10 +36,21 @@ while True:
     (cnts,_) = cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 70000:
             continue
+        status = 1
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+    
+    status_list.append(status)
+    
+    
+    if status_list[-1] == 1 and status_list[-2] == 0:
+        times.append(datetime.now())
+    
+    if status_list[-1] == 0 and status_list[-2] == 1:
+        times.append(datetime.now())
+
 
 
     # thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
@@ -52,14 +70,28 @@ while True:
     #     first_frame = gray
 
     key = cv2.waitKey(1)
-    print(gray)
-    print(delta_frame)
+    #print(gray)
+    #print(delta_frame)
 
     if key == ord('q'):
+        if status == 1:
+            times.append(datetime.now())
         break
     
+    #print(status)
+    
+print(status_list)
+print(times)
 
+rows = []
+for i in range(0, len(times), 2):
+    rows.append({"Start": times[i], "End": times[i+1]})
 
+df = pandas.DataFrame(rows)
+df.to_csv("Times.csv", index = False)
+df.to_json("Times.json", index = False)     
+
+   
 #print(f'Number of frames {a}')
 video.release()
 cv2.destroyAllWindows()
